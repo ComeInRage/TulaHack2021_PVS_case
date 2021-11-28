97a6eb8c9f213b3d62a1d822c7552f86cba643ec
ComeInRage <montekek011@mail.ru>
2021-11-28 12:49:34 +0300

    Updated example

--- examples/example.cpp
+++ examples/example.cpp
@@ -43,14 +43,24 @@ int main() {
 	std::cout << "Changed:" << std::endl;
 	for (const auto& warning_pair : changes.changed_warnings) {
 
-		// Мои глаза...
-
 		auto extra_has_code = warning_pair.first.at_key("code").get<std::string>();
 		auto absent_has_code = warning_pair.second.at_key("code").get<std::string>();
+
 		auto extra_has_msg = warning_pair.first.at_key("message").get<std::string>();
 		auto absent_has_msg = warning_pair.second.at_key("message").get<std::string>();
-		std::cout << "Was: [" << (absent_has_code ? *absent_has_code : "null") << "]: " << (absent_has_msg ? *absent_has_msg : "null") << "\n"
-				  << "Now: [" << (extra_has_code ? *extra_has_code : "null") << "]: " << (extra_has_msg ? *extra_has_msg : "null") << std::endl;
+
+		auto extra_has_file_path = warning_pair.first.at_key("positions").at_key(0).at_key("file").get<std::string>();
+		auto absent_has_file_path = warning_pair.second.at_key("positions").at_key(0).at_key("file").get<std::string>();
+
+		auto extra_has_line_number = warning_pair.first.at_key("positions").at_key(0).at_key("line").get<int>();
+		auto absent_has_line_number = warning_pair.second.at_key("positions").at_key(0).at_key("line").get<int>();
+
+		std::cout << "Was: [" << (absent_has_code ? *absent_has_code : "null") << "] line " << (absent_has_line_number ? *absent_has_line_number : -1) 
+				  << ": " << (absent_has_msg ? *absent_has_msg : "null") << "\n"
+				  << "Path: " << (absent_has_file_path ? *absent_has_file_path : "null_path") << "\n"
+				  << "Now: [" << (extra_has_code ? *extra_has_code : "null") << "] line " << (extra_has_line_number ? *extra_has_line_number : -1) 
+				  << ": " << (extra_has_msg ? *extra_has_msg : "null") << "\n"
+				  << "Path: " << (extra_has_file_path ? *extra_has_file_path : "null_path") << std::endl;
 
 	}
 
